c522943ad927693970073acfe52c7668e26ef0e6
liminal18 <aljones15@gmail.com>
2017-05-07 21:34:56 +0700

    updated to work with stack

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PathFinder
+
+stack setup
+stack build
+if that fails try stack build cabal-install
+then stack solver --update-config
+also try stack install c2hs
+stack exec PathFinder-exec
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
 import Lib
+import UI.NCurses
+import Control.Monad
+import Control.Monad.IO.Class
 
 main :: IO ()
-main = someFunc
+main = do
+  tiles <- constructTerrian rows columns
+  directions <- constructCompass rows columns
+  runCurses $ do
+    setEcho False
+    w <- defaultWindow
+    updateWindow w $ do
+      drawTitle 1 1 breadthFirstTitle
+      mapCursorText (toInteger rows) 1 2 tiles
+      mapCursorText (toInteger rows) 30 2 directions
+    render
+    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
